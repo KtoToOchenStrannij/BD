b09e0418c01f2f37a92258f618a102bd97544073
KtoToOchenStrannij <sorokinaanya69@mail.ru>
2022-10-09 17:39:47 +0300

    Наконец-то

---
+++ mainwindow.cpp
@@ -0,0 +1,48 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName("./mydb.db");
+    if(db.open())
+    {
+        qDebug("open");
+    }
+    else
+    {
+        qDebug("no open");
+    }
+
+    query = new QSqlQuery(db);
+    query->exec("CREATE TABLE Numbers(Name TEXT, Number INT);");
+    model = new QSqlTableModel(this, db);
+    model->setTable("Numbers");
+    model->select();
+
+    ui->tableView->setModel(model);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    model->insertRow(model->rowCount());
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    model->removeRow(row);
+}
+
+void MainWindow::on_tableView_clicked(const QModelIndex &index)
+{
+    row = index.row();
+}
